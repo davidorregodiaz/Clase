6965d5b60af8cbd8718572acfc64014b84f49bd9
davidorregodiaz <orregod091@gmail.com>
2024-04-27 16:49:06 -0400

    commit del repo

--- Clase.java
+++ Clase.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Clase{
 
     public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
         System.out.println("Hola mundo");
+        System.out.println("Commit");
+        System.out.println("Digite un valor: ");
+        int valor = entrada.nextInt();
+        System.out.println(valor);
     }
 }
